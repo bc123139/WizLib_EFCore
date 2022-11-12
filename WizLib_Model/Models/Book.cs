c964c9f61b9726671755aa17d59e444324e45474
Usman Tahir <Usman.tahir@synergy-it.com>
2022-11-12 18:05:03 +0500

    Update book and category

--- WizLib_Model/Models/Book.cs
+++ WizLib_Model/Models/Book.cs
@@ -12,5 +12,7 @@ namespace WizLib_Model.Models
         public string ISBN { get; set; }
         [Required]
         public double Price { get; set; }
+        public int CategoryId { get; set; }
+        public Category Category { get; set; } = null!;
     }
 }
