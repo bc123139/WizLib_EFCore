fdce919b22a82050001fbdeba55f4b152d908dc3
Usman Tahir <Usman.tahir@synergy-it.com>
2022-11-14 23:26:07 +0500

    Create Update Fetch Records

---
+++ WizLib/Controllers/CategoryController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WizLib_DataAccess.Data;
+using WizLib_Model.Models;
+
+namespace WizLib.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            List<Category> objList = _db.Categories.AsNoTracking().ToList();
+            return View(objList);
+        }
+
+
+        public IActionResult Upsert(int? id)
+        {
+            Category obj = new();
+            if (id == null)
+            {
+                return View(obj);
+            }
+            //this for edit
+            obj = _db.Categories.FirstOrDefault(u => u.Id == id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            return View(obj);
+        }
+    }
+}
